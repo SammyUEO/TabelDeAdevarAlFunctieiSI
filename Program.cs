2e71d322bace577a6e05093f5eff1f4ab4b4b2bd
SammyUEO <146414814+SammyUEO@users.noreply.github.com>
2023-10-13 08:57:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("| A     | B     | A AND B |");
+        Console.WriteLine("|-------|-------|---------|");
+        Console.WriteLine("| true  | true  | " + (true & true) + "     |");
+        Console.WriteLine("| true  | false | " + (true & false) + "    |");
+        Console.WriteLine("| false | true  | " + (false & true) + "    |");
+        Console.WriteLine("| false | false | " + (false & false) + "   |");
+    }
+}
